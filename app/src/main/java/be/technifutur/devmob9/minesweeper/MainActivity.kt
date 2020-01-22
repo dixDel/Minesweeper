1bf101041135ec294ed2e5aebd519d429745354e
Didier Delhaye <didier.delhaye@drkdidel.be>
2020-01-22 21:43:57 +0100

    display board

--- app/src/main/java/be/technifutur/devmob9/minesweeper/MainActivity.kt
+++ app/src/main/java/be/technifutur/devmob9/minesweeper/MainActivity.kt
@@ -1,12 +1,36 @@
 package be.technifutur.devmob9.minesweeper
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val nbCol = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setupBoard()
+    }
+
+    fun setupBoard() {
+        for (i in 0..this.nbCol) {
+            val button = Button(this)
+            button.text = "1"
+            val layoutParams = ViewGroup.LayoutParams(
+                //100, 100
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            button.layoutParams = layoutParams
+            boardLayout.addView(button)
+        }
     }
 }
